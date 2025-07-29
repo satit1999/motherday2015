2669a278fbe373a3ef6c3de58aac625f7e2f7832
satit1999 <sstsatanan@gmail.com>
2025-07-29 21:31:09 +0700

    Add files via upload

---
+++ components/icon/SchoolLogoIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const SchoolLogoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    fill="none" 
+    viewBox="0 0 24 24" 
+    strokeWidth={1.5} 
+    stroke="currentColor" 
+    {...props}
+  >
+    <path 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      d="M4.26 10.147a60.438 60.438 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0l-.07.042m15.482 0l.07.042m-4.59 2.073a36.42 36.42 0 00-1.042-.146A36.42 36.42 0 0012 9.75c-1.348 0-2.684.068-4.009.201m8.018 0l-.07.042m-8.018 0l.07.042" 
+    />
+  </svg>
+);
